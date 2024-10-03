34eb675a6ee9c6fa0139e645853a1413fd2cde49
cholaraju <89002217+cholaraju@users.noreply.github.com>
2024-10-03 15:04:55 +0530

    added lafoot

--- .github/workflows/chola-script.sh
+++ .github/workflows/chola-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f elephant"tharun fucked elephat Hemanth" >>dragon.txt
+cowsay -f elephant "tharun fucked elephat Hemanth" >> dragon.txt
 grep -i "elephant" dragon.txt
 cat dragon.txt
 ls -ltra
